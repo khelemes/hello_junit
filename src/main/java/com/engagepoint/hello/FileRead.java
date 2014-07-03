1a48e97d365d156a21ab1586504d6164b8be6313
Leonid Khelemes <leonid.helemes@i.ua>
2014-07-04 00:35:19 +0300

    File read added

--- src/main/java/com/engagepoint/hello/FileRead.java
+++ src/main/java/com/engagepoint/hello/FileRead.java
@@ -14,17 +14,32 @@ public class FileRead {
                 throws FileNotFoundException, IOException {
 
             File file = new File( "G:\\Kak_dobavit_knigi.txt" );
-
-            BufferedReader br = new BufferedReader (
+            BufferedReader br=null;
+            try {
+            br = new BufferedReader (
                     new InputStreamReader(
                             new FileInputStream( file ), "windows-1251"
                     )
             );
+            } catch (FileNotFoundException e) {
+                System.out.println("File \"" + file + "\" not found");
+                return;
+            }
             String line = null;
+            try
+            {
             while ((line = br.readLine()) != null) {
                 //variable line does NOT have new-line-character at the end
                 System.out.println( line );
             }
-            br.close();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                br.close();
+            }
         }
     }
