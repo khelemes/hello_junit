228cea985dc90d0b0d44eb3b8ec13f9d7595671e
Leonid Khelemes <leonid.helemes@i.ua>
2014-07-08 00:10:47 +0300

    Thread added

--- src/main/java/com/engagepoint/hello/Program.java
+++ src/main/java/com/engagepoint/hello/Program.java
@@ -2,12 +2,13 @@ package com.engagepoint.hello;
 
 /*import net.schmizz.sshj.*;*/
 
+import java.awt.List;
 import java.io.IOException;
 
 
 import com.jcraft.jsch.*;
 import java.awt.*;
-import java.io.InputStream;
+import java.util.*;
 import javax.swing.*;
 
 /**
@@ -16,91 +17,22 @@ import javax.swing.*;
 public class Program {
     public static void main(String[] arg) {
 
-/*//Graph objects tests
+
         Point p=new Point();
-*//*        p.x=10;
-        p.y=32;*//*
+        p.x=10;
+        p.y=32;
         p.draw();
 
 
-        */
+        java.util.List<Integer> l=new ArrayList<Integer>();
+        l.add(10);
+        System.out.println("ddd");
+        System.out.println(l.get(0));
+        System.out.println(l.get(1));
 
 
         //ssh
-
-
-        try {
-            JSch jsch = new JSch();
-            String user="root";
-            String passwd="wasadmin";
-            String passwd2[] =
-                    {
-                            "This",
-                            "was5CHT",
-                            "was4CHT",
-                            "wasadmin",
-                            "root"
-                    };
-
-            String host="192.168.34.20";
-            String command="echo Hello ssh!";
-            Session session=jsch.getSession(user, host, 22);
-            session.setPassword(passwd);
-            session.getHostKey();
-            java.util.Properties config = new java.util.Properties();
-            config.put("StrictHostKeyChecking", "no");
-            session.setConfig(config);
-
-            for (String str:passwd2){
-                System.out.printf("Trying password %s for host %s\n", str, host);
-                session.setPassword(str);
-                session.connect();
-            }
-            session.connect();
-            Channel channel=session.openChannel("exec");
-            ((ChannelExec)channel).setCommand(command);
-
-            //channel.setInputStream(System.in);
-            channel.setInputStream(null);
-
-            ((ChannelExec)channel).setErrStream(System.err);
-
-            InputStream in=channel.getInputStream();
-
-            channel.connect();
-
-            byte[] tmp=new byte[1024];
-            while(true){
-                while(in.available()>0){
-                    int i=in.read(tmp, 0, 1024);
-                    if(i<0)break;
-                    System.out.print(new String(tmp, 0, i));
-                }
-                if(channel.isClosed()){
-                    if(in.available()>0) continue;
-                    System.out.println("exit-status: "+channel.getExitStatus());
-                    break;
-                }
-                try{Thread.sleep(1000);}catch(Exception ee){}
-            }
-            channel.disconnect();
-            session.disconnect();
-
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
-
-
-
-
-
-
-
-
-
-     /*   //work
-        try {
+ /*       try {
             JSch jsch = new JSch();
             String user="root";
             String passwd="wasadmin";
@@ -122,8 +54,6 @@ public class Program {
             channel.setOutputStream(System.out);
 
             channel.connect(3*1000);
-
-
 
 
             System.out.println("It works!") ;
---
+++ src/main/java/com/engagepoint/hello/TestThread.java
@@ -0,0 +1,74 @@
+package com.engagepoint.hello;
+
+/**
+ * Created by lyolik on 07.07.2014.
+ */
+public class TestThread implements Runnable{
+    public void run() {
+        for (int i=0; i<10; i++) {
+            System.out.println("Hello from a thread!");
+        }
+    }
+
+    public static void main(String args[]) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<10; i++) {
+                    System.out.println("Hello from a thread 1!");
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<10; i++) {
+                    System.out.println("Hello from a thread 2!");
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<10; i++) {
+                    System.out.println("Hello from a thread 3!");
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<10; i++) {
+                    System.out.println("Hello from a thread 4!");
+                    try {
+                        Thread.sleep(4000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
+/*        (new Thread(new TestThread())).start();
+        (new Thread(new TestThread())).start();*/
+    }
+
+}
